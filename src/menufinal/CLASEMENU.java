f307585924c6f99b6c702d365e9921c8a56a35be
evers <evers@LAPTOP-02KD6DNJ.home>
2019-11-06 17:41:16 -0600

    CONTIENE TODO EL MENU PERO LE FALTAN LOS LLAMADOS

--- src/menufinal/CLASEMENU.java
+++ src/menufinal/CLASEMENU.java
@@ -1,68 +1,57 @@
 
 package menufinal;
 
-import javax.swing.JOptionPane;
-
 public class CLASEMENU {
+    public int numero1;
+    public int numero2;
+    public double resultado;
+  
     
-    private int id;
-    private String fecha;
-    private String nombre;
-
-    public int getid() {
-        return id;
-    }
-
-    public void setid(int id) {
-        this.id = id;
+    public int getnumero1() {
+        return numero1;
     }
 
-    public String getfecha() {
-        return fecha;
+    public void setnumero1(int numero1) {
+        this.numero1 = numero1;
+    }
+ 
+    public int getnumero2() {
+        return numero2;
     }
 
-    public void setfecha(String fecha) {
-        this.fecha = fecha;
+    public void setnumero2(int numero2) {
+        this.numero2 = numero2;
+    }
+    
+    public double getresultado() {
+        return resultado;
     }
 
-    public String getnombre() {
-        return nombre;
-    }
-
-    public void setnombre(String nombre) {
-        this.nombre = nombre;
+    public void setresultado(double resultado) {
+        this.resultado = resultado;
     }
     
-    public void commit(){
-        JOptionPane.showMessageDialog(null, " hacer un commit se refiere, en el contexto de la ciencia de la \n  computación y la gestión de datos, a la idea de confirmar un conjunto de cambios provisionales de forma \n permanente. Un uso popular es al final de una transacción de base de datos.");
+    public int Sumar(int numero1, int numero2){
+        this.setnumero1(numero1);
+        this.setnumero2(numero2); 
+        return this.getnumero1()+this.getnumero2();
+  }
+   
+   public int Restar(int num1, int num2){
+         this.setnumero1(num1);
+        this.setnumero2(num2); 
+        return this.getnumero1()-this.getnumero2();
+    }
+   
+   public int Multiplicar(int num1, int num2){
+         this.setnumero1(num1);
+        this.setnumero2(num2); 
+        return this.getnumero1()*this.getnumero2();
     }
     
-    public void push(){
-        JOptionPane.showMessageDialog(null, "  Esta operación toma como argumento un registro general, de \n segmento o una posición de memoria y la introduce (copia) al Stack.");
-    }
-    
-    public void pull(){
-        JOptionPane.showMessageDialog(null, "Los pull request permiten no solo llevar de forma más ordenada las \n tareas en la etapa del desarrollo, sino también crear propuestas o cambios que puedan \n ser integrados posteriormente a dicho proyecto.");
-    }
-    
-    public void clase(){
-        JOptionPane.showMessageDialog(null, "una clase es una plantilla para la creación de objetos de datos según \n un modelo predefinido. Las clases se utilizan para representar entidades o conceptos,\n como los sustantivos en el lenguaje. Cada clase es un modelo que define un conjunto\n de variables -el estado, y métodos apropiados para operar con dichos datos -el comportamiento. Cada objeto creado a \n partir de la clase se denomina instancia de la clase.");
-    }
-    
-    public void objeto(){
-        JOptionPane.showMessageDialog(null, "un objeto es una unidad dentro de un programa de computadores \n que consta de un estado y de un comportamiento, que a su vez constan respectivamente \n  de datos almacenados y de tareas realizables durante el tiempo de ejecución. Un objeto \n puede ser creado instanciando una clase, como ocurre en la programación orientada a objetos, o mediante escritura directa de código y la replicación de \n otros objetos, como ocurre en la programación basada en prototipos.");
+   public double Division(int num1, int num2){
+         this.setnumero1(num1);
+        this.setnumero2(num2); 
+        return (double) getnumero1()/this.getnumero2();
     }
 }
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
--- src/menufinal/MENUFINAL.java
+++ src/menufinal/MENUFINAL.java
@@ -1,21 +1,25 @@
 
 package menufinal;
 
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 
 public class MENUFINAL {
 
     public static void main(String[] args) {
-      int op=0;
-        int opcion=0;
-        CLASEMENU obj = new CLASEMENU ();
+      int op = 0;
+      int opcion = 0;
+      int sumar = 0;
+      int numero1;
+      int numero2;
+      CLASEMENU obj = new CLASEMENU ();
                 
            
         do{  
           try{  
             
-   opcion = Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor de la operacion que quiere realizar"
+                opcion = Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor de la operacion que quiere realizar"
                     + "\n 1.  OPERACIONES"
                     + "\n 2.  ARREGLOS"
                     + "\n 3.  MATRICES"
@@ -28,25 +32,86 @@ public class MENUFINAL {
                     + "\n 10. ARBOLES"
                     + "\n 11. GRAFOS"
                     + "\n 12. SALIR"));
-       
-          }catch(Exception ex){
-              JOptionPane.showMessageDialog(null,"Debe ingresar solo  valores numericos");
-          }
+           }catch(Exception ex){JOptionPane.showMessageDialog(null,"Debe ingresar solo  valores numericos"); }
+          
+          
             switch(opcion){
-                case 1:
-                    obj.commit();
-                   break; 
+                case 1: 
+                do{ 
+                   try{ 
+                           op = Integer.parseInt(JOptionPane.showInputDialog("elija la operacion"
+                           + "\n 1.  SUMA"
+                           + "\n 2.  RESTA"
+                           + "\n 3.  MULTIPLICACION"
+                           + "\n 4.  DIVISION"
+                           + "\n 5.  SALIR"));
+                    }catch(Exception ex){JOptionPane.showMessageDialog(null,"Debe ingresar solo  valores numericos"); }
+                    
+                    
+                    switch(op){
+                     case 1:
+                        numero1=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor del primer numero"));
+                        numero2=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor del segundo numero"));
+                        
+                    break;
+                     case 2:
+                     JOptionPane.showMessageDialog(null, "DS");
+                    break;
+                    case 3:    
+                     JOptionPane.showMessageDialog(null, "DS");
+                    break;
+                    case 4:
+                     JOptionPane.showMessageDialog(null, "DS");
+                    break;
+                    case 5:
+                     JOptionPane.showMessageDialog(null, "DS");
+                    break;
+                    default:
+                    JOptionPane.showMessageDialog(null, "Ingresó una opción no valida");
+                   break;   
+                    }
+                    
+                }while(opcion != 5);    
+                
+                    break;
                 case 2:
-                    obj.push ();
+                
+                   JOptionPane.showMessageDialog(null, "DS");
                     break;
                 case 3:
-                    obj.pull();
+                JOptionPane.showMessageDialog(null, "DS");
                     break;
                 case 4:
-                    obj.clase();
+                JOptionPane.showMessageDialog(null, "DS");
                     break;
                 case 5:
-                    obj.objeto();
+                    Scanner entrada = new Scanner(System.in);
+                    int arreglo[],nElementos;
+                    int aux;
+        
+                    nElementos = Integer.parseInt(JOptionPane.showInputDialog("digite la cantidad de elemtos"));
+        
+                    arreglo = new int[nElementos];
+        
+                   for(int i=0;i<nElementos;i++){
+                    System.out.print((i+1)+".digite un numero");
+                    arreglo[i] = entrada.nextInt();
+                    }
+        
+                    for(int i=0;i<(nElementos-1);i++){
+                     for(int j=0;j<(nElementos-1);j++){
+                      if(arreglo[j] > arreglo[j+1]){
+                        aux = arreglo[j];
+                        arreglo[j] = arreglo[j+1];
+                        arreglo[j+1] = aux;  
+                        }
+                       }
+                      }
+        
+                      System.out.println("\nArreglo ordenado en forma creciente:");
+                     for(int i=0;i<nElementos;i++){
+                      System.out.print(arreglo[i]+" - ");
+                      }
                     break;  
                 case 6:
                     JOptionPane.showMessageDialog(null, "DS");
@@ -71,20 +136,8 @@ public class MENUFINAL {
                     break;  
                 default:
                     JOptionPane.showMessageDialog(null, "Ingresó una opción no valida");
-                   break;
-            }
-            }while(opcion != 13);
+                   break;   
+          }     
+        }while(opcion != 12);
     }
-    
 }
-        
-        
-        
-        
-        
-        
-        
-        
-    
-    
-
